b43809311037c4b023369feb43c3d3fe7cacf19a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-03-17 18:05:51 -0300

    Remove license present in every file

--- src/Support/SqlInteractive.php
+++ src/Support/SqlInteractive.php
@@ -1,25 +1,5 @@
 <?php
 
-/**
- * Part of the Sqli package.
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the 3-clause BSD License.
- *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
- *
- * @package    Sqli
- * @version    0.1.0
- * @author     Antonio Carlos Ribeiro @ PragmaRX
- * @license    BSD License (3-clause)
- * @copyright  (c) 2013, PragmaRX
- * @copyright  (c) 2009, Ian Eure <ieure@php.net> *** This class is based on PHPRepl
- * @link       http://pragmarx.com
- */
-
 namespace PragmaRX\Sqli\Support;
 
 use PragmaRX\Sqli\Support\DatabaseConnection;
